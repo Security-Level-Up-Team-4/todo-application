d13c84894f47ae3c4fc8efab94670f918d83a31f
Alfred Malope <alfred.malope@vcontractor.co.za>
2025-06-04 13:47:40 +0200

    Add Budget alert

---
+++ cdk/bin/cdk.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { CdkStack } from '../lib/cdk-stack';
+
+const app = new cdk.App();
+
+const recipients = [
+  'Alfred.Malope@bbd.co.za',
+  'Vuyo.Sibiya@bbd.co.za',
+  'Ryan.Christie@bbd.co.za',
+  'Tshepo.Ncube@bbd.co.za',
+  'Rorisang.Shadung@bbd.co.za'
+]
+
+new CdkStack(app, 'TodoCdkStack-prod', {
+  stackName: 'team4-aws-infrastructure',
+  description: 'Team 4 - AWS CDK Stack',
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: "af-south-1",
+  },
+  alertEmails: recipients,
+});
