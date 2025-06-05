33d257df50af6002725359a98cad8ce0721a07db
Ryan Christie <ryan.christie@bbd.co.za>
2025-06-05 22:48:04 +0200

    Mock integration for admin page

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,13 @@
+const Loader = () => {
+     let circleCommonClasses = 'h-2.5 w-2.5 bg-current rounded-full';
+
+     return (
+    <div className='flex m-auto'>
+         <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
+         <div className={`${circleCommonClasses} mr-1 animate-bounce-200`}></div>
+         <div className={`${circleCommonClasses} animate-bounce-400`}></div>
+    </div>
+     );
+};
+
+export default Loader;
--- frontend/src/models/user.ts
+++ frontend/src/models/user.ts
@@ -1,12 +1,20 @@
 export interface User {
   id: number;
   username: string;
+  email?: string;
+  role?: string;
+}
+
+export enum UserRoles {
+  ADMIN = "Admin",
+  TEAMLEAD = "Teamlead",
+  TODO = "Todo",
 }
 
 export const mockUsers: User[] = [
-  { username: "Ryan", id: 0},
-  { username: "Tshepo", id: 1 },
-  { username: "Alfred", id: 2 },
-  { username: "Vuyo", id: 3 },
-  { username: "Rorisang", id: 4 },
+  { id: 0, username: "Ryan", email:"ryan@bbd.co.za", role: UserRoles.ADMIN },
+  { id: 1, username: "Tshepo", email:"tshepo@bbd.co.za", role: UserRoles.TEAMLEAD },
+  { id: 2, username: "Alfred", email:"alfred@bbd.co.za", role: UserRoles.TODO },
+  { id: 3, username: "Vuyo", email:"vuyo@bbd.co.za", role: UserRoles.TODO },
+  { id: 4, username: "Rorisang", email:"rorisang@bbd.co.za", role: UserRoles.TEAMLEAD },
 ];
